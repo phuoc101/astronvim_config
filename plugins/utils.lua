df6057f7c9fcbaea1c87e9640b28a609e6f058be
phuoc101 <nguyenthuanphuoc101@protonmail.com>
2024-03-27 21:57:41 +0200

    add neogen for documentation

--- plugins/utils.lua
+++ plugins/utils.lua
@@ -8,4 +8,10 @@ return {
     "junegunn/fzf.vim",
     event = "VeryLazy",
   },
+  {
+    "danymat/neogen",
+    ft = { "python", "c", "cpp" },
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function() require("neogen").setup { snippet_engine = "luasnip" } end,
+  },
 }
